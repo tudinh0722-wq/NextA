879e7452848175b30c6d4456d2d92c0eb6193622
tudinh0722-wq <tudinh0722@gmail.com>
2026-09-08 11:35:31 +0700

    Connect add event screen to activity

--- app/src/main/java/com/nexta/MainActivity.kt
+++ app/src/main/java/com/nexta/MainActivity.kt
@@ -5,11 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.lifecycleScope
 import com.nexta.data.repository.EventRepository
 import com.nexta.data.sample.SampleDataSeeder
+import com.nexta.ui.AddEventScreen
 import com.nexta.ui.MainScreen
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -19,10 +22,12 @@ class MainActivity : ComponentActivity() {
     @Inject
     lateinit var repository: EventRepository
 
+    private var showAddEvent by mutableStateOf(false)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        MainScope().launch {
+        lifecycleScope.launch {
             SampleDataSeeder.seedIfEmpty(repository)
         }
 
@@ -31,9 +36,26 @@ class MainActivity : ComponentActivity() {
                 .getAllEvents()
                 .collectAsState(initial = emptyList())
 
-            MainScreen(
-                events = events
-            )
+            if (showAddEvent) {
+                AddEventScreen(
+                    onBack = {
+                        showAddEvent = false
+                    },
+                    onSave = { event ->
+                        lifecycleScope.launch {
+                            repository.saveEvent(event)
+                            showAddEvent = false
+                        }
+                    }
+                )
+            } else {
+                MainScreen(
+                    events = events,
+                    onAddEvent = {
+                        showAddEvent = true
+                    }
+                )
+            }
         }
     }
 }
