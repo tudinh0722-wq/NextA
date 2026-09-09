04a5cb4c647cec27de131646e324c771bb576edb
tudinh0722-wq <tudinh0722@gmail.com>
2026-09-09 11:33:58 +0700

    Support atomic bulk event insert

--- app/src/main/java/com/nexta/data/local/EventDao.kt
+++ app/src/main/java/com/nexta/data/local/EventDao.kt
@@ -5,18 +5,10 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface EventDao {
-    @Query("SELECT * FROM events")
-    fun getAll(): Flow<List<EventEntity>>
-
-    @Query("SELECT * FROM events WHERE id = :id")
-    suspend fun getById(id: String): EventEntity?
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(event: EventEntity)
-
-    @Update
-    suspend fun update(event: EventEntity)
-
-    @Query("DELETE FROM events WHERE id = :id")
-    suspend fun delete(id: String)
+    @Query("SELECT * FROM events") fun getAll(): Flow<List<EventEntity>>
+    @Query("SELECT * FROM events WHERE id = :id") suspend fun getById(id: String): EventEntity?
+    @Insert(onConflict = OnConflictStrategy.REPLACE) suspend fun insert(event: EventEntity)
+    @Insert(onConflict = OnConflictStrategy.REPLACE) suspend fun insertAll(events: List<EventEntity>)
+    @Update suspend fun update(event: EventEntity)
+    @Query("DELETE FROM events WHERE id = :id") suspend fun delete(id: String)
 }
