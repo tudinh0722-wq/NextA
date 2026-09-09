0b7fd651fbb1325e8868f516237b6d0bf1f89392
tudinh0722-wq <tudinh0722@gmail.com>
2026-09-09 16:56:27 +0700

    refactor: persist event alarms in Room database

--- app/src/main/java/com/nexta/data/local/AppDatabase.kt
+++ app/src/main/java/com/nexta/data/local/AppDatabase.kt
@@ -4,11 +4,12 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(
-    entities = [EventEntity::class],
-    version = 2,
+    entities = [EventEntity::class, EventAlarmEntity::class],
+    version = 3,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
-
     abstract fun eventDao(): EventDao
+    abstract fun alarmDao(): AlarmDao
+    abstract fun eventScheduleDao(): EventScheduleDao
 }
